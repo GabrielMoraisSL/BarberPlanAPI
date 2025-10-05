10601b01246f5c08cdb3c31f04d8bbabf3218ba4
gmorais <gmorais@fatecie.edu.br>
2025-10-05 19:12:20 -0300

    fix: deploy vercel

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import express from 'express';
+import * as express from 'express';
 
 const server = express();
 
